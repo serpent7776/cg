4f69b9e78f6b504ce030ad359f4043ebeff7016f
Serpent7776 <Serpent7776@gmail.com>
2023-04-23 22:43:09 +0200

    Add mappings for vi mode in fish

--- activate.fish
+++ activate.fish
@@ -1,3 +1,5 @@
 #!/usr/bin/env fish
 set _p (status dirname)
 bind \cg 'set _d ("$_p/g.fish") && cd "$_d" && fish_prompt'
+bind --mode insert \cg 'set _d ("$_p/g.fish") && cd "$_d" && fish_prompt'
+bind --mode normal \cg 'set _d ("$_p/g.fish") && cd "$_d" && fish_prompt'
