869b71c90dd9effe6df2246d78ed0ef56e5b3318
Serpent7776 <Serpent7776@gmail.com>
2023-04-24 18:06:28 +0200

    Remove shebang from activate scripts
    They need to be sourced anyway.

--- activate.csh
+++ activate.csh
@@ -1,3 +1,2 @@
-#!/usr/bin/env tcsh
 set _p = `dirname $1`
 bindkey -sb "^G" 'set _d = `tcsh "$_p/g.csh"` && cd $_d^M'
--- activate.fish
+++ activate.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 set _p (status dirname)
 bind \cg 'set _d ("$_p/g.fish") && cd "$_d" && fish_prompt'
 bind --mode insert \cg 'set _d ("$_p/g.fish") && cd "$_d" && fish_prompt'
