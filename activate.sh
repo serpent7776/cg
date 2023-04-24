54e54d86d6dd95a6294069c6add2dfa815ac4593
Serpent7776 <Serpent7776@gmail.com>
2023-04-24 16:45:12 +0200

    Add support for bash and zsh

--- activate.sh
+++ activate.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env sh
 _p=$(dirname $0)
-echo "bind -s ^G '_d=\$(sh \"$_p/g.sh\") && cd \$_d^M'"
+if [ -n "`echo $ZSH_VERSION`"  ]; then
+	echo "bindkey -s '^G' '_d=\$(sh \"$_p/g.sh\") && cd \$_d^M'"
+elif [ -n "`echo $BASH_VERSINFO`"  ]; then
+	echo "bind '\"\C-G\": \". $_p/cg.sh $_p\n\"'"
+else
+	# Assuming bourne shell
+	echo "bind -s ^G '_d=\$(sh \"$_p/g.sh\") && cd \$_d^M'"
+fi
